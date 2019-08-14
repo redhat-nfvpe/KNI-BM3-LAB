b168845fbb4dde4e39887266eee026350e889a74
atyronesmith <aasmith@redhat.com>
2019-08-14 09:59:33 -0400

    Added gen_matchbox.sh

--- upi/common.sh
+++ upi/common.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+OPENSHIFT_RHCOS_MAJOR_REL="4.1"
+export OPENSHIFT_RHCOS_MAJOR_REL
+
+OPENSHIFT_RHCOS_MINOR_REL="4.1.0"
+export OPENSHIFT_RHCOS_MINOR_REL
+
+OPENSHIFT_RHCOS_URL="https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/$OPENSHIFT_RHCOS_MAJOR_REL/$OPENSHIFT_RHCOS_MINOR_REL"
+export OPENSHIFT_RHCOS_URL
+
+
 PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export PROJECT_DIR
 
